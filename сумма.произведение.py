3f6c53483984309498e37f28dc125442de05e082
Tartar <101659868+YuliaTar@users.noreply.github.com>
2022-03-22 16:24:17 +0300

    Add files via upload

---
+++ сумма.произведение.py
@@ -0,0 +1,8 @@
+uno = int(input('Введите первое число:'))
+dos = int(input('Введие второе число'))
+
+if uno + dos <= 1000:
+    print(f'Сумма {uno} и {dos} = {uno + dos}.')
+else:
+    print(f'Произведение {uno} и {dos} = {uno * dos}')
+    
